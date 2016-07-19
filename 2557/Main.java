d582d85b75394b19d056825373ff3b36684beda3
Kim Min-Ho <minho1a@hanmail.net>
2016-07-19 15:45:23 +0900

    https://www.acmicpc.net/problem/2557

---
+++ 2557/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.07.19
+ * email: minho1a@hanmail.net
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/2557
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
