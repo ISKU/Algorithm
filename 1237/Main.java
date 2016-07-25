f790bfa2396894d80fdfdc2d67107f0b6a71eef6
Kim Min-Ho <minho1a@hanmail.net>
2016-07-26 07:35:14 +0900

    https://www.acmicpc.net/problem/1237

---
+++ 1237/Main.java
@@ -0,0 +1,14 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.07.26
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/1237
+ */
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("문제의 정답");
+    }
+}
