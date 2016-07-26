881d91adc745c8e744293b9ade0a9b85ed3252ad
Kim Min-Ho <minho1a@hanmail.net>
2016-07-26 20:20:57 +0900

    https://www.acmicpc.net/problem/5338

---
+++ 5338/Main.java
@@ -0,0 +1,18 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.07.26
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/5338
+ */
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+}
