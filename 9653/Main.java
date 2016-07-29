97c5fbf2a5c6818a66f2da60eb95204b1b968ec9
Kim Min-Ho <minho1a@hanmail.net>
2016-07-29 23:14:29 +0900

    https://www.acmicpc.net/problem/9653

---
+++ 9653/Main.java
@@ -0,0 +1,24 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.07.29
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/9653
+ */
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("    8888888888  888    88888");
+		System.out.println("   88     88   88 88   88  88");
+		System.out.println("    8888  88  88   88  88888");
+		System.out.println("       88 88 888888888 88   88");
+		System.out.println("88888888  88 88     88 88    888888");
+		System.out.println();
+		System.out.println("88  88  88   888    88888    888888");
+		System.out.println("88  88  88  88 88   88  88  88");
+		System.out.println("88 8888 88 88   88  88888    8888");
+		System.out.println(" 888  888 888888888 88  88      88");
+		System.out.println("  88  88  88     88 88   88888888");
+	}
+}
