db88c1047c71a8c1cea6093f94a2ba9d77a5c851
Kim Min-Ho <minho1a@hanmail.net>
2016-08-02 01:05:10 +0900

    https://www.acmicpc.net/problem/10699

---
+++ 10699/Main.java
@@ -0,0 +1,17 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.08.02
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/10699
+ */
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(new Date()).toString());
+	}
+}
