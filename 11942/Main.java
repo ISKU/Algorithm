3113c6cf75adfe72a6e55f6d93760adc74567964
Kim Min-Ho <minho1a@hanmail.net>
2016-08-02 00:46:51 +0900

    https://www.acmicpc.net/problem/11942

---
+++ 11942/Main.java
@@ -0,0 +1,14 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.08.02
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/11942
+ */
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("고려대학교");
+	}
+}
