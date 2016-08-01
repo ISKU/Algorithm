43830118a3f9c4d62a2c0b6d209add581f5d9673
Kim Min-Ho <minho1a@hanmail.net>
2016-08-02 00:43:15 +0900

    https://www.acmicpc.net/problem/11654

---
+++ 11654/Main.java
@@ -0,0 +1,17 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.08.02
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/11654
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		System.out.println((int) input.nextLine().charAt(0));
+	}
+}
