7f6747b07717c285fe1d2ae2bdce4e4c68ae27a7
Kim Min-Ho <minho1a@hanmail.net>
2016-08-02 11:07:02 +0900

    https://www.acmicpc.net/problem/10718

---
+++ 10718/Main.java
@@ -0,0 +1,14 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.08.02
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/10718
+ */
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.printf("강한친구 대한육군\n강한친구 대한육군");
+	}
+}
