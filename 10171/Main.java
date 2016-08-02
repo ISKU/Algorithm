3303679118c92a8e0e47c24f58a1f27700d92bb1
Kim Min-Ho <minho1a@hanmail.net>
2016-08-02 21:31:09 +0900

    https://www.acmicpc.net/problem/10171

---
+++ 10171/Main.java
@@ -0,0 +1,17 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.07.31
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/10171
+ */
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
