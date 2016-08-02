ef8b6c43e42406c74a0e0beac489909d245a9f86
Kim Min-Ho <minho1a@hanmail.net>
2016-08-02 21:28:44 +0900

    https://www.acmicpc.net/problem/10172

---
+++ 10172/Main.java
@@ -0,0 +1,18 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.07.31
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/10172
+ */
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
