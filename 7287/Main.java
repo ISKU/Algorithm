4a485e400c7e7a95de6fa78f22da535a1dea1d62
Kim Min-Ho <minho1a@hanmail.net>
2016-08-03 23:51:41 +0900

    https://www.acmicpc.net/problem/7287

---
+++ 7287/Main.java
@@ -0,0 +1,15 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.08.03
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/7287
+ */
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("104");
+		System.out.println("isku");
+	}
+}
