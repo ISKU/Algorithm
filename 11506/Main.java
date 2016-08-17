f95ab0ed7504c8c324084e3bf76c3e1260a7735a
Kim Min-Ho <minho1a@hanmail.net>
2016-08-17 20:20:32 +0900

    https://www.acmicpc.net/problem/11506

---
+++ 11506/Main.java
@@ -0,0 +1,14 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.08.17
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/11506
+ */
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("EFBFBD"); // closed
+	}
+}
