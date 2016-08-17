c06711a6d309ad39fae2e983a082031080a778b2
Kim Min-Ho <minho1a@hanmail.net>
2016-08-17 19:37:04 +0900

    https://www.acmicpc.net/problem/9999

---
+++ 9999/Main.java
@@ -0,0 +1,14 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.08.17
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/9999
+ */
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("**********"); // closed
+	}
+}
