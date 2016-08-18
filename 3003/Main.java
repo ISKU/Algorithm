e8ae3942e0ef69c40a959548eac46a14ac85d250
Kim Min-Ho <minho1a@hanmail.net>
2016-08-18 23:39:59 +0900

    https://www.acmicpc.net/problem/3003

---
+++ 3003/Main.java
@@ -0,0 +1,22 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.08.18
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/3003
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		System.out.print((1 - input.nextInt()) + " ");
+		System.out.print((1 - input.nextInt()) + " ");
+		System.out.print((2 - input.nextInt()) + " ");
+		System.out.print((2 - input.nextInt()) + " ");
+		System.out.print((2 - input.nextInt()) + " ");
+		System.out.print((8 - input.nextInt()));
+	}
+}
