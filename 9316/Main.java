df08c68c0ca1895f9761c740b4fbeefb12299ab7
Kim Min-Ho <minho1a@hanmail.net>
2016-08-18 23:44:26 +0900

    https://www.acmicpc.net/problem/9316

---
+++ 9316/Main.java
@@ -0,0 +1,18 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.08.18
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/9316
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		int count = new Scanner(System.in).nextInt();
+		for (int index = 1; index <= count; index++)
+			System.out.printf("Hello World, Judge %d!\n", index);
+	}
+}
