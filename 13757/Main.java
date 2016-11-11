c43d797a18ff0021d18b14273909dd1afd875cf4
Kim Min-Ho <minho1a@hanmail.net>
2016-11-11 12:35:15 +0900

    https://www.acmicpc.net/problem/13757

---
+++ 13757/Main.java
@@ -0,0 +1,14 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2016.11.11
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/13757
+ */
+
+public class Main {
+	public static void main(String... args) {
+        System.out.println("빼빼로 먹고 싶어요!");
+    }
+}
