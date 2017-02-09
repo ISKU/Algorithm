31f5141a9e173be603160065c2b11b8228a4ab89
Kim Min-Ho <minho1a@hanmail.net>
2017-02-09 07:59:22 +0000

    https://www.acmicpc.net/problem/10170

---
+++ 10170/Main.java
@@ -0,0 +1,26 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.02.09
+ * email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/10170
+ */
+
+public class Main {
+	public static void main(String... args) {
+		System.out.println("NFC West       W   L  T");
+		System.out.println("-----------------------");
+		System.out.println("Seattle        13  3  0");
+		System.out.println("San Francisco  12  4  0");
+		System.out.println("Arizona        10  6  0");
+		System.out.println("St. Louis      7   9  0");
+		System.out.println("");
+		System.out.println("NFC North      W   L  T");
+		System.out.println("-----------------------");
+		System.out.println("Green Bay      8   7  1");
+		System.out.println("Chicago        8   8  0");
+		System.out.println("Detroit        7   9  0");
+		System.out.println("Minnesota      5  10  1");
+	}
+}
