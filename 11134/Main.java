495c93035a4c2eaccf677cbf7b4411608d46606b
Kim Min-Ho <minho1a@hanmail.net>
2017-02-13 18:43:23 +0000

    https://www.acmicpc.net/problem/11134

---
+++ 11134/Main.java
@@ -0,0 +1,23 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.02.14
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/11134
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String... args) {
+		Scanner input = new Scanner(System.in);
+		int testCase = input.nextInt();
+
+		while (testCase-- > 0) {
+			int N = input.nextInt();
+			int C = input.nextInt();
+			System.out.println(N % C == 0 ? N / C : N / C + 1);
+		}
+	}
+}
