79e6058ef9a9272523cf53eff6541d371a6bca11
Kim Min-Ho <minho1a@hanmail.net>
2017-02-14 02:59:21 +0000

    https://www.acmicpc.net/problem/9659

---
+++ 9659/Main.java
@@ -0,0 +1,16 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.02.14
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/9659
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print(new Scanner(System.in).nextLong() % 2 == 0 ? "CY" : "SK");
+	}
+}
