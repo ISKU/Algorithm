11f97f2392dec63459d22749629b3a5e03fad593
Kim Min-Ho <minho1a@hanmail.net>
2017-02-14 13:14:06 +0000

    https://www.acmicpc.net/problem/9654

---
+++ 9654/Main.java
@@ -0,0 +1,19 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.02.14
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/9654
+ */
+
+public class Main {
+	public static void main(String... args) {
+		System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+		System.out.println("N2 Bomber      Heavy Fighter  Limited    21        ");
+		System.out.println("J-Type 327     Light Combat   Unlimited  1         ");
+		System.out.println("NX Cruiser     Medium Fighter Limited    18        ");
+		System.out.println("N1 Starfighter Medium Fighter Unlimited  25        ");
+		System.out.println("Royal Cruiser  Light Combat   Limited    4         ");
+	}
+}
