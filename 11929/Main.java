e899bfaa94c95a0eb70c3b154c45e0eb7bb9f15a
Kim Min-Ho <minho1a@hanmail.net>
2017-02-14 19:22:47 +0000

    https://www.acmicpc.net/problem/11929

---
+++ 11929/Main.java
@@ -0,0 +1,14 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.02.14
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/11929
+ */
+
+public class Main {
+    public static void main(String... args) {
+        
+    }
+}
