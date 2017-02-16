820913c0fcad9f5b02fb41dbd023a7ab185a1700
Kim Min-Ho <minho1a@hanmail.net>
2017-02-16 05:49:17 +0000

    https://www.acmicpc.net/problem/2476

---
+++ 2476/Main.java
@@ -0,0 +1,50 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.02.16
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/2476
+ */
+
+import java.util.Scanner;
+import java.util.Hashtable;
+import java.util.Enumeration;
+
+public class Main {
+	public static void main(String... args) {
+		Scanner input = new Scanner(System.in);
+		int testCase = input.nextInt();
+		int max = Integer.MIN_VALUE;
+
+		while (testCase-- > 0) {
+			Hashtable<Integer, Integer> table = new Hashtable<Integer, Integer>();
+
+			for (int i = 0; i < 3; i++) {
+				int cur = input.nextInt();
+				table.put(cur, table.containsKey(cur) ? table.get(cur) + 1 : 1);
+			}
+
+			int tempMax = 0;
+			int freq = 0;
+			int freqKey = 0;
+			for (Enumeration<Integer> keys = table.keys(); keys.hasMoreElements();) {
+				int key = keys.nextElement();
+				if (freq < table.get(key)) {
+					freqKey = key;
+					freq = table.get(key);
+				}
+				tempMax = Math.max(tempMax, key);
+			}
+
+			if (table.size() == 1)
+				max = Math.max(max, 10000 + (freqKey * 1000));
+			else if (table.size() == 2)
+				max = Math.max(max, 1000 + (freqKey * 100));
+			else
+				max = Math.max(max, tempMax * 100);
+		}
+
+		System.out.println(max);
+	}
+}
