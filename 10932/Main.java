4b9e85d23cc953e98d3f7492bf4e9366121c50e7
Kim Min-Ho <minho1a@hanmail.net>
2017-02-22 22:18:34 +0000

    https://www.acmicpc.net/problem/10932

---
+++ 10932/Main.java
@@ -0,0 +1,25 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.02.23
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/10932
+ */
+
+import java.util.Scanner;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Main {
+	public static void main(String... args) throws NoSuchAlgorithmException {
+		MessageDigest sh = MessageDigest.getInstance("SHA-512");
+		sh.update(new Scanner(System.in).next().getBytes());
+		StringBuffer output = new StringBuffer();
+
+		for (byte b : sh.digest())
+			output.append(Integer.toHexString(0xf & b >> 4) + Integer.toHexString(0xf & b));
+
+		System.out.println(output);
+	}
+}
