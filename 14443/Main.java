1f30422829409f92e7a5820570e8cf7835f76948
Kim Min-Ho <minho1a@hanmail.net>
2017-02-23 05:21:50 +0000

    https://www.acmicpc.net/problem/14443

---
+++ 14443/Main.java
@@ -0,0 +1,14 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.02.23
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/14443
+ */
+
+public class Main {
+	public static void main(String... args) {
+			System.out.print("prime");
+	}
+}
