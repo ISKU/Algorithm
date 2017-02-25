57d30326f7e7b4a247f6f720b76bf860eb422d29
Kim Min-Ho <minho1a@hanmail.net>
2017-02-25 12:20:49 +0000

    https://www.acmicpc.net/problem/13239

---
+++ 13239/Main.java
@@ -0,0 +1,28 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.02.25
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/13239
+ */
+
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String... args) {
+		Scanner input = new Scanner(System.in);
+		int testCase = input.nextInt();
+
+		while (testCase-- > 0)
+			System.out.println(combination(input.nextInt(), input.nextInt()));
+	}
+
+	private static BigInteger combination(int n, int k) {
+		BigInteger result = BigInteger.ONE;
+		for (int i = 0; i < k; i++)
+			result = result.multiply(BigInteger.valueOf(n - i)).divide(BigInteger.valueOf(i + 1));
+		return result.remainder(BigInteger.valueOf(1000000007));
+	}
+}
