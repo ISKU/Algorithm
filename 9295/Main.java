4b5deed4e02cb4014474db0a7ad1f9712bdcac31
Kim Min-Ho <minho1a@hanmail.net>
2017-02-28 16:12:25 +0000

    https://www.acmicpc.net/problem/9295

---
+++ 9295/Main.java
@@ -0,0 +1,20 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.03.01
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/9295
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String... args) {
+		Scanner input = new Scanner(System.in);
+		int testCase = input.nextInt();
+
+		for (int x = 1; x <= testCase; x++)
+			System.out.printf("Case %d: %d\n", x, input.nextInt() + input.nextInt());
+	}
+}
