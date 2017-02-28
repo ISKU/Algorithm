0bcbd5edecd2088d183f24c961915eb8415cb3c2
Kim Min-Ho <minho1a@hanmail.net>
2017-02-28 16:22:10 +0000

    https://www.acmicpc.net/problem/10179

---
+++ 10179/Main.java
@@ -0,0 +1,22 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.03.01
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/10179
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String... args) {
+		Scanner input = new Scanner(System.in);
+		int testCase = input.nextInt();
+
+		while (testCase-- > 0) {
+			double cost = input.nextDouble();
+			System.out.printf("$%.2f\n", cost - (cost * 0.2));
+		}
+	}
+}
