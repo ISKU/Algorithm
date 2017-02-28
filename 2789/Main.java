1b893717ec96cfe477c8f0413231e04a201f908f
Kim Min-Ho <minho1a@hanmail.net>
2017-02-28 10:44:38 +0000

    https://www.acmicpc.net/problem/2789

---
+++ 2789/Main.java
@@ -0,0 +1,16 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.02.28
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/2789
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print(new Scanner(System.in).next().replaceAll("[CAMBRIDGE]", ""));
+	}
+}
