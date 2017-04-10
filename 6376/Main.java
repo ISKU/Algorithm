369bae1698496a2b5208b729d5a71cde1dafc108
Kim Min-Ho <minho1a@hanmail.net>
2017-04-10 10:21:53 +0000

    https://www.acmicpc.net/problem/6376

---
+++ 6376/Main.java
@@ -0,0 +1,25 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.04.10
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/6376
+ */
+
+public class Main {
+	public static void main(String... args) {
+		System.out.println("n e");
+		System.out.println("- -----------");
+		System.out.println("0 1");
+		System.out.println("1 2");
+		System.out.println("2 2.5");
+		System.out.println("3 2.666666667");
+		System.out.println("4 2.708333333");
+		System.out.println("5 2.716666667");
+		System.out.println("6 2.718055556");
+		System.out.println("7 2.718253968");
+		System.out.println("8 2.718278770");
+		System.out.println("9 2.718281526");
+	}
+}
