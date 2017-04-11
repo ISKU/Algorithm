22f77daaec5e8497242e2308f4d0784673552145
Kim Min-Ho <minho1a@hanmail.net>
2017-04-11 11:29:51 +0000

    https://www.acmicpc.net/problem/11722

---
+++ 11722/Main.java
@@ -0,0 +1,35 @@
+/* 
+ * Author: Kim Min-Ho (ISKU)
+ * Date: 2017.04.11
+ * Email: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/11722
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String... args) {
+		Scanner input = new Scanner(System.in);
+		int[] array = new int[input.nextInt()];
+		int[] table = new int[array.length];
+
+		for (int i = 0; i < array.length; i++)
+			array[i] = input.nextInt();
+
+		for (int i = 0; i < array.length; i++) {
+			table[i] = 1;
+
+			for (int j = 0; j < i; j++)
+				if (array[i] < array[j] && table[j] + 1 > table[i])
+					table[i] = table[j] + 1;
+		}
+
+		int max = 0;
+		for (int i = 0; i < array.length; i++)
+			max = Math.max(max, table[i]);
+
+		System.out.print(max);
+	}
+}
