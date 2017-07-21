385d87b8bf9d34d96eaf7545f4e937a67e6c6d86
Minho Kim <minho1a@hanmail.net>
2017-07-21 16:38:55 +0900

    BOJ #14645: 와이버스 부릉부릉

---
+++ BOJ/14645/Main.java
@@ -0,0 +1,14 @@
+/* 
+ * Author: Minho Kim (ISKU)
+ * Date: 2017.07.21
+ * E-mail: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/14645
+ */
+
+public class Main {
+	public static void main(String... args) {
+		System.out.printf("비와이");
+	}
+}
