882977dff9ead83e2cffe9aa9a5ede3a08a0ea40
Minho Kim <minho1a@hanmail.net>
2017-08-04 11:13:07 +0900

    BOJ #13328: Message Passing

---
+++ BOJ/13328/Main.java
@@ -0,0 +1,49 @@
+/* 
+ * Author: Minho Kim (ISKU)
+ * Date: 2017.08.03
+ * E-mail: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/13328
+ */
+
+import java.util.*;
+
+public class Main {
+
+	private static long MOD = 31991;
+	private static int d;
+
+	public static void main(String... args) {
+		Scanner input = new Scanner(System.in);
+		d = input.nextInt();
+		int t = input.nextInt();
+		long[][] D = new long[d][d];
+		long[][] answer = new long[d][d];
+
+		for (int i = 0; i < d; i++)
+			D[0][i] = answer[i][i] = 1;
+		for (int i = 1; i < d; i++)
+			D[i][i - 1] = 1;
+
+		while (t > 0) {
+			if (t % 2 == 1)
+				answer = multiply(D, answer);
+			D = multiply(D, D);
+			t /= 2;
+		}
+
+		System.out.print(answer[0][0]);
+	}
+
+	private static long[][] multiply(long[][] A, long[][] B) {
+		long[][] ret = new long[d][d];
+
+		for (int i = 0; i < d; i++)
+			for (int j = 0; j < d; j++)
+				for (int k = 0; k < d; k++)
+					ret[i][j] = (ret[i][j] + (A[i][k] * B[k][j]) % MOD) % MOD;
+
+		return ret;
+	}
+}
