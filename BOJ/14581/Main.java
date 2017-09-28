e77cceba82521ce298338fc2b9b1ef9dd025ad39
Minho Kim <minho1a@hanmail.net>
2017-09-28 22:07:27 +0900

    BOJ #14581: 팬들에게 둘러싸인 홍준

---
+++ BOJ/14581/Main.java
@@ -0,0 +1,18 @@
+/* 
+ * Author: Minho Kim (ISKU)
+ * Date: 2017.09.28
+ * E-mail: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/14581
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String... args) {
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + new Scanner(System.in).next() + "::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+}
