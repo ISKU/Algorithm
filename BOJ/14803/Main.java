afd56c427d154b2950ec211f0c17b4beef74171f
Minho Kim <minho1a@hanmail.net>
2017-10-12 02:52:29 +0900

    BOJ #14803: Steed 2: Cruise Control (Small)

---
+++ BOJ/14803/Main.java
@@ -0,0 +1,28 @@
+/* 
+ * Author: Minho Kim (ISKU)
+ * Date: 2017.04.22
+ * E-mail: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/14803
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String... args) {
+		Scanner input = new Scanner(System.in);
+		int testCase = input.nextInt();
+
+		for (int tc = 1; tc <= testCase; tc++) {
+			double D = input.nextDouble();
+			int N = input.nextInt();
+
+			double max = 0;
+			for (int i = 0; i < N; i++)
+				max = Math.max(max, (D - input.nextDouble()) / input.nextDouble());
+
+			System.out.printf("Case #%d: %.6f\n", tc, D / max);
+		}
+	}
+}
