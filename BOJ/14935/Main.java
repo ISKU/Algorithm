5a0eb8bfcdccaf05ef657b842098d50864a2a515
Minho Kim <minho1a@hanmail.net>
2017-12-18 18:40:48 +0900

    BOJ #14935: FA

---
+++ BOJ/14935/Main.java
@@ -0,0 +1,14 @@
+/* 
+ * Author: Minho Kim (ISKU)
+ * Date: 2017.12.18
+ * E-mail: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/14935
+ */
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print("FA");
+	}
+}
