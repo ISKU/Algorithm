8469e086f2c08abffa897a8c1b4ab26e43fa8125
Minho Kim <minho1a@hanmail.net>
2017-12-18 18:44:49 +0900

    BOJ #14918: 더하기

---
+++ BOJ/14918/Main.java
@@ -0,0 +1,17 @@
+/* 
+ * Author: Minho Kim (ISKU)
+ * Date: 2017.12.18
+ * E-mail: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/14918
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String... args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print(input.nextInt() + input.nextInt());
+	}
+}
