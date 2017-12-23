b571df28be2e7c3f52469c5af073f7cd06fcfce2
Minho Kim <minho1a@hanmail.net>
2017-12-23 19:59:08 +0900

    BOJ #15272: Hissing Microphone

---
+++ BOJ/15272/Main.java
@@ -0,0 +1,17 @@
+/* 
+ * Author: Minho Kim (ISKU)
+ * Date: 2017.12.23
+ * E-mail: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15272
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String... args) {
+		String s = new Scanner(System.in).next();
+		System.out.print(s.contains("ss") ? "hiss" : "no hiss");
+	}
+}
