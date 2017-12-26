62466d93f47c789276f029d4656a9268b9c12a37
Minho Kim <minho1a@hanmail.net>
2017-12-26 23:00:44 +0900

    BOJ #15000: CAPS

---
+++ BOJ/15000/Main.java
@@ -0,0 +1,16 @@
+/* 
+ * Author: Minho Kim (ISKU)
+ * Date: 2017.12.26
+ * E-mail: minho1a@hanmail.net
+ * 
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15000
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print(new Scanner(System.in).next().toUpperCase());
+	}
+}
