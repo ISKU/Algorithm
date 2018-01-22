1dc218ecc99d04080a4e420c07c2c93fd8e3590a
Minho Kim <minho1a@hanmail.net>
2018-01-22 18:45:22 +0900

    BOJ #11284: 초성 중성 종성

---
+++ BOJ/11284/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: January 22, 2018
+ * E-mail: minho1a@hanmail.net
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/11284
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String... args) {
+		int letter = new Scanner(System.in).next().charAt(0) - 0xAC00;
+		String[] first = { "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ" };
+		String[] second = { "ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ" };
+		String[] thrid = { "", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ" };
+		System.out.println(first[letter / 21 / 28]);
+		System.out.println(second[(letter % (21 * 28)) / 28]);
+		System.out.println(thrid[letter % 28]);
+	}
+}
