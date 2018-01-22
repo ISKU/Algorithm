4897bcfe6fa7e2cb3e72b81feb83a7d23604b594
Minho Kim <minho1a@hanmail.net>
2018-01-22 18:06:59 +0900

    BOJ #11283: 한글 2

---
+++ BOJ/11283/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: January 22, 2018
+ * E-mail: minho1a@hanmail.net
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/11283
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print((int) (new Scanner(System.in).next().charAt(0) - 0xABFF));
+	}
+}
