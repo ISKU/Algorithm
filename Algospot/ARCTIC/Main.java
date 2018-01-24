355b3704766a49e044715604a853d72e805a1a81
Minho Kim <minho1a@hanmail.net>
2018-01-24 13:55:28 +0900

    Algospot: ARCTIC

--- Algospot/ARCTIC/Main.java
+++ Algospot/ARCTIC/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
-public class ARCTIC {
+public class Main {
 	public static void main(String... args) {
 		Scanner input = new Scanner(System.in);
 		int testCase = input.nextInt();
