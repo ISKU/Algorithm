325f6cefd66d1c034ba2fb7b0f49f574b8653053
Minho Kim <minho1a@hanmail.net>
2018-01-24 13:55:48 +0900

    Algospot: JAEHASAFE

--- Algospot/JAEHASAFE/Main.java
+++ Algospot/JAEHASAFE/Main.java
@@ -10,7 +10,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class JAEHASAFE {
+public class Main {
 	public static void main(String... args) {
 		Scanner input = new Scanner(System.in);
 		int testCase = input.nextInt();
