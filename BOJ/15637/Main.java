e210c62d887b2db258c149a3c7401c9e70ffc48d
Minho Kim <minho.kim093@gmail.com>
2018-04-02 23:35:59 +0900

    BOJ #15637: Lotto

---
+++ BOJ/15637/Main.java
@@ -0,0 +1,113 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: April 1, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15637
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print(get(new Scanner(System.in).nextInt()));
+	}
+
+	private static int get(int N) {
+		switch (N) {
+		case 1:
+			return 103;
+		case 2:
+			return 88;
+		case 3:
+			return 91;
+		case 4:
+			return 99;
+		case 5:
+			return 101;
+		case 6:
+			return 86;
+		case 7:
+			return 96;
+		case 8:
+			return 100;
+		case 9:
+			return 71;
+		case 10:
+			return 91;
+		case 11:
+			return 96;
+		case 12:
+			return 91;
+		case 13:
+			return 98;
+		case 14:
+			return 102;
+		case 15:
+			return 96;
+		case 16:
+			return 85;
+		case 17:
+			return 102;
+		case 18:
+			return 97;
+		case 19:
+			return 91;
+		case 20:
+			return 112;
+		case 21:
+			return 85;
+		case 22:
+			return 79;
+		case 23:
+			return 86;
+		case 24:
+			return 91;
+		case 25:
+			return 97;
+		case 26:
+			return 98;
+		case 27:
+			return 110;
+		case 28:
+			return 80;
+		case 29:
+			return 88;
+		case 30:
+			return 80;
+		case 31:
+			return 95;
+		case 32:
+			return 83;
+		case 33:
+			return 93;
+		case 34:
+			return 109;
+		case 35:
+			return 88;
+		case 36:
+			return 91;
+		case 37:
+			return 103;
+		case 38:
+			return 90;
+		case 39:
+			return 95;
+		case 40:
+			return 113;
+		case 41:
+			return 81;
+		case 42:
+			return 86;
+		case 43:
+			return 97;
+		case 44:
+			return 93;
+		case 45:
+			return 93;
+		default:
+			return 0;
+		}
+	}
+}
