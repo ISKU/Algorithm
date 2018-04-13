fbc536db7c9f8a91b8c60356f7f22ce3f2ec7b3c
Minho Kim <minho.kim093@gmail.com>
2018-04-13 22:31:20 +0900

    BOJ #11660: 구간 합 구하기 5

---
+++ BOJ/11660/Main.java
@@ -0,0 +1,39 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: April 13, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/11660
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[][] psum = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++)
+				psum[i][j] += Integer.parseInt(st.nextToken()) + psum[i - 1][j] + psum[i][j - 1] - psum[i - 1][j - 1];
+		}
+
+		while (M-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			bw.write((psum[x2][y2] - psum[x1 - 1][y2] - psum[x2][y1 - 1] + psum[x1 - 1][y1 - 1]) + "\n");
+		}
+
+		bw.close();
+	}
+}
