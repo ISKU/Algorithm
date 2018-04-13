76cf07e62cecd0b6820ee53e3ae78b8cb9c6674a
Minho Kim <minho.kim093@gmail.com>
2018-04-13 22:36:42 +0900

    BOJ #15650: N과 M (2)

---
+++ BOJ/15650/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: April 13, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15650
+ */
+
+import java.util.*;
+
+public class Main {
+
+	private static int N, M;
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+
+		dfs(0, 1, "");
+	}
+
+	private static void dfs(int step, int n, String result) {
+		if (step >= M) {
+			System.out.println(result);
+			return;
+		}
+
+		for (int i = n; i <= N; i++)
+			dfs(step + 1, i + 1, result + i + " ");
+	}
+}
