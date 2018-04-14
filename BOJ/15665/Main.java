3a1c19b4077dc23d451f29fba97cbbf7baf483b6
Minho Kim <minho.kim093@gmail.com>
2018-04-14 23:46:42 +0900

    BOJ #15665: N과 M (11)

---
+++ BOJ/15665/Main.java
@@ -0,0 +1,45 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: April 14, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15665
+ */
+
+import java.util.*;
+
+public class Main {
+
+	private static int N, M;
+	private static int[] array;
+	private static HashSet<String> set;
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+
+		array = new int[N];
+		for (int i = 0; i < N; i++)
+			array[i] = sc.nextInt();
+
+		Arrays.sort(array);
+		set = new HashSet<String>();
+		dfs(0, 0, "");
+	}
+
+	private static void dfs(int s, int n, String result) {
+		if (s >= M) {
+			if (set.contains(result))
+				return;
+
+			set.add(result);
+			System.out.println(result);
+			return;
+		}
+
+		for (int i = 0; i < N; i++)
+			dfs(s + 1, i + 1, result + array[i] + " ");
+	}
+}
