bec6845341dab7797433752f138c6a66f5c3683e
Minho Kim <minho.kim093@gmail.com>
2018-04-20 20:14:28 +0900

    BOJ #13277: 큰 수 곱셈

---
+++ BOJ/13277/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: April 19, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/13277
+ */
+
+import java.util.*;
+import java.math.*;
+
+public class Main {
+	public static void main(String... args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print(new BigInteger(sc.next()).multiply(new BigInteger(sc.next())));
+	}
+}
