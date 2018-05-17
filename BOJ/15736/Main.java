462f713bc36f888d2f290e22bd52cc16d3dc287d
Minho Kim <minho.kim093@gmail.com>
2018-05-18 06:30:13 +0900

    BOJ #15736: 청기 백기

---
+++ BOJ/15736/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: May 16, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15736
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print((int) Math.sqrt(new Scanner(System.in).nextLong()));
+	}
+}
