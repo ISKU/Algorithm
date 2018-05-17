6111cffdae3188b4cfa919cb393f637dd642653c
Minho Kim <minho.kim093@gmail.com>
2018-05-18 06:18:26 +0900

    BOJ #15733: 나는 누구인가

---
+++ BOJ/15733/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: May 16, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15733
+ */
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print("I'm Sexy");
+	}
+}
