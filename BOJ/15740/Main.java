e7b8581a75157e70a11704f89c7002a52d6359d3
Minho Kim <minho.kim093@gmail.com>
2018-05-23 22:09:59 +0900

    BOJ #15740: A+B - 9

---
+++ BOJ/15740/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: May 22, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15740
+ */
+
+import java.util.*;
+import java.math.*;
+
+public class Main {
+	public static void main(String... args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print(new BigInteger(sc.next()).add(new BigInteger(sc.next())));
+	}
+}
