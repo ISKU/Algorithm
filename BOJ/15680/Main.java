2d3b9521a15ac8099553e918c4eba243d2abcd6a
Minho Kim <minho.kim093@gmail.com>
2018-05-27 20:29:12 +0900

    BOJ #15680: 연세대학교

---
+++ BOJ/15680/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: May 22, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15680
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print(new Scanner(System.in).nextInt() == 0 ? "YONSEI" : "Leading the Way to the Future");
+	}
+}
