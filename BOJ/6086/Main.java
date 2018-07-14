8524012381bdb519ef884683dbc790fad89311e5
Minho Kim <minho.kim093@gmail.com>
2018-07-14 18:05:22 +0900

    BOJ #6086: 최대 유량

--- BOJ/6086/Main.java
+++ BOJ/6086/Main.java
@@ -1,57 +1,57 @@
-/* 
- * Author: Kim Min-Ho (ISKU)
- * Date: 2017.02.25
- * Email: minho1a@hanmail.net
- * 
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: July 14, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
  * https://github.com/ISKU/Algorithm
  * https://www.acmicpc.net/problem/6086
  */
 
-import java.util.Scanner;
-import java.util.Vector;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	private static Vector<Vector<Integer>> graph;
+	private static ArrayList<Integer>[] network;
 	private static int[][] capacity;
 	private static int[][] flow;
 
-	public static void main(String... args) {
-		Scanner input = new Scanner(System.in);
-		int N = input.nextInt();
+	public static void main(String... args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 
 		capacity = new int[52][52];
 		flow = new int[52][52];
-		graph = new Vector<Vector<Integer>>();
+		network = new ArrayList[52];
 		for (int i = 0; i < 52; i++)
-			graph.add(new Vector<Integer>());
+			network[i] = new ArrayList<Integer>();
 
 		while (N-- > 0) {
-			int u = ctoi(input.next().charAt(0));
-			int v = ctoi(input.next().charAt(0));
-			int w = input.nextInt();
-			graph.get(u).add(v);
-			graph.get(v).add(u);
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int u = ctoi(st.nextToken().charAt(0));
+			int v = ctoi(st.nextToken().charAt(0));
+			int w = Integer.parseInt(st.nextToken());
+			network[u].add(v);
+			network[v].add(u);
 			capacity[u][v] += w;
+			capacity[v][u] += w;
 		}
 
 		int total = 0;
-		int source = ctoi('A'), sink = ctoi('Z');
+		int source = ctoi('A');
+		int sink = ctoi('Z');
 		while (true) {
-			Queue<Integer> queue = new LinkedList<Integer>();
+			Queue<Integer> q = new LinkedList<Integer>();
 			int[] prev = new int[52];
 			Arrays.fill(prev, -1);
 
-			queue.add(source);
-			while (!queue.isEmpty()) {
-				int cur = queue.poll();
+			q.add(source);
+			while (!q.isEmpty()) {
+				int cur = q.poll();
 
-				for (int next : graph.get(cur)) {
+				for (int next : network[cur]) {
 					if (capacity[cur][next] - flow[cur][next] > 0 && prev[next] == -1) {
-						queue.add(next);
+						q.add(next);
 						prev[next] = cur;
 
 						if (next == sink)
