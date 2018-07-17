62df1658c2b1ba64eee3ef379085d69f83351219
Minho Kim <minho.kim093@gmail.com>
2018-07-17 11:33:22 +0900

    BOJ #15890: 전국 대학생 프로그래밍 대회 동아리 연합 여름 대회 2018

---
+++ BOJ/15890/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: July 14, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15890
+ */
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print("20180729");
+	}
+}
