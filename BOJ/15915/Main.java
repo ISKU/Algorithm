7de9291cbd9f82aeeea8d2bd428336d395d27cfa
Minho Kim <minho.kim093@gmail.com>
2018-07-17 11:35:21 +0900

    BOJ #15915: 가위 바위 보 1002

---
+++ BOJ/15915/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: July 17, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15915
+ */
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print(0);
+	}
+}
