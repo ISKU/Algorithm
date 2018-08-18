ffe163a5296ae919a719b05d784402a004a23c04
Minho Kim <minho.kim093@gmail.com>
2018-08-18 21:09:31 +0900

    BOJ #15963: CASIO

---
+++ BOJ/15963/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: August 14, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15963
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print(sc.next().equals(sc.next()) ? 1 : 0);
+	}
+}
