4e943a034755b123e696bdcc39e7f7946d426a38
Minho Kim <minho.kim093@gmail.com>
2018-08-18 21:06:40 +0900

    BOJ #15962: 새로운 시작

---
+++ BOJ/15962/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: August 14, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15962
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("파이팅!!");
+	}
+}
