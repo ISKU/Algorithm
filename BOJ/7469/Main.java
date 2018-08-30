50bf29273986c7420dd1080b16cd802871f3e8c5
Minho Kim <minho.kim093@gmail.com>
2018-08-30 13:34:15 +0900

    BOJ #7469: K번째 수

--- BOJ/7469/Main.java
+++ BOJ/7469/Main.java
@@ -1,107 +1,596 @@
-/* 
- * Author: Kim Min-Ho (ISKU)
- * Date: 2016.08.07
- * email: minho1a@hanmail.net
- * 
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: August 30, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
  * https://github.com/ISKU/Algorithm
  * https://www.acmicpc.net/problem/7469
  */
 
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
 
-	private static Node[] mainArray;
-	private static int arrayMaxSize;
+	private static int[][] tree;
+	private static int H;
 
-	public static void main(String args[]) throws IOException {
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer parser = new StringTokenizer(input.readLine());
-		StringBuilder output = new StringBuilder(); // output, k-th number
-		arrayMaxSize = Integer.parseInt(parser.nextToken());
-		int numberOfCalls = Integer.parseInt(parser.nextToken());
-		mainArray = new Node[arrayMaxSize];
+	private static void solve() {
+		int N = io.nextInt();
+		int Q = io.nextInt();
 
-		// input, Node(value, index)
-		parser = new StringTokenizer(input.readLine());
-		for (int index = 0; index < arrayMaxSize; index++)
-			mainArray[index] = new Node(Integer.parseInt(parser.nextToken()), index);
+		H = 1 << (int) Math.ceil(Math.log(N) / Math.log(2));
+		tree = new int[H * 2][];
+		for (int i = 0; i < N; i++)
+			tree[i + H] = new int[] { io.nextInt() };
+		for (int i = H + N; i < tree.length; i++)
+			tree[i] = new int[] { Integer.MAX_VALUE };
+		build(1, H, 1);
 
-		quickSort(mainArray, 0, arrayMaxSize - 1); // quick sort 
-
-		// output, it — the k-th number in sorted a[i...j] segment
-		for (int count = 0; count < numberOfCalls; count++) {
-			parser = new StringTokenizer(input.readLine());
-			output.append(
-					getArrayValue(
-							Integer.parseInt(parser.nextToken()), 
-							Integer.parseInt(parser.nextToken()), 
-							Integer.parseInt(parser.nextToken())) + "\n"
-			);
-		}
-		System.out.print(output);
+		while (Q-- > 0)
+			io.println(query(io.nextInt(), io.nextInt(), io.nextInt()));
 	}
 
-	private static int getArrayValue(int first, int last, int returnIndex) {
-		int returnIndexCount = 0;
-		int returnValue = 0; // k-th number
+	private static int query(int L, int R, int K) {
+		int l = 0;
+		int r = tree[1].length - 1;
 
-		for (int index = 0; index < arrayMaxSize; index++) {
-			if ((mainArray[index].index >= first - 1) && (mainArray[index].index <= last - 1)) {
-				returnIndexCount++; // find k-th number
-				if (returnIndexCount == returnIndex) {
-					returnValue = mainArray[index].value;
-					break;
-				}
+		while (l < r) {
+			int mid = (l + r) / 2;
+
+			if (count(1, H, 1, L, R, tree[1][mid]) < K)
+				l = mid + 1;
+			else
+				r = mid;
+		}
+
+		return tree[1][l];
+	}
+
+	private static int count(int l, int r, int i, int L, int R, int K) {
+		if (r < L || R < l)
+			return 0;
+		if (L <= l && r <= R)
+			return upperBound(tree[i], K);
+
+		int mid = (l + r) / 2;
+		return count(l, mid, i * 2, L, R, K) + count(mid + 1, r, i * 2 + 1, L, R, K);
+	}
+
+	private static void build(int l, int r, int i) {
+		if (i >= H)
+			return;
+		int mid = (l + r) / 2;
+		build(l, mid, i * 2);
+		build(mid + 1, r, i * 2 + 1);
+
+		int[] left = tree[i * 2];
+		int[] right = tree[i * 2 + 1];
+		tree[i] = new int[left.length + right.length];
+		int p = 0;
+		int pl = 0;
+		int pr = 0;
+
+		while (pl < left.length && pr < right.length) {
+			if (left[pl] < right[pr])
+				tree[i][p++] = left[pl++];
+			else
+				tree[i][p++] = right[pr++];
+		}
+		while (pl < left.length)
+			tree[i][p++] = left[pl++];
+		while (pr < right.length)
+			tree[i][p++] = right[pr++];
+	}
+
+	private static int upperBound(int[] array, int K) {
+		int l = 0;
+		int r = array.length - 1;
+
+		while (l < r) {
+			int mid = (l + r) / 2;
+
+			if (array[mid] <= K)
+				l = mid + 1;
+			else
+				r = mid;
+		}
+
+		return (array[l] <= K) ? l + 1 : l;
+	}
+
+	private static FastIO io;
+
+	public static void main(String... args) {
+		io = new FastIO(System.in, System.out);
+		solve();
+		io.close();
+	}
+
+	/**
+	 * https://github.com/ISKU/FastIO-Java
+	 * 
+	 * @author Minho Kim <minho.kim093@gmail.com>
+	 */
+	private static class FastIO {
+
+		public static final int DEFAULT_BUFFER_SIZE = 65536;
+		public static final int DEFAULT_INTEGER_SIZE = 10;
+		public static final int DEFAULT_LONG_SIZE = 19;
+		public static final int DEFAULT_WORD_SIZE = 256;
+		public static final int DEFAULT_LINE_SIZE = 1024;
+		public static final int EOF = -1;
+
+		private final InputStream in;
+		private final OutputStream out;
+
+		private byte[] inBuffer;
+		private int nextIn, inLength;
+		private byte[] outBuffer;
+		private int nextOut;
+
+		private char[] charBuffer;
+		private byte[] byteBuffer;
+
+		public FastIO(InputStream in, OutputStream out, int inBufferSize, int outBufferSize) {
+			this.in = in;
+			this.inBuffer = new byte[inBufferSize];
+			this.nextIn = 0;
+			this.inLength = 0;
+
+			this.out = out;
+			this.outBuffer = new byte[outBufferSize];
+			this.nextOut = 0;
+
+			this.charBuffer = new char[DEFAULT_LINE_SIZE];
+			this.byteBuffer = new byte[DEFAULT_LONG_SIZE];
+		}
+
+		public FastIO(InputStream in, OutputStream out) {
+			this(in, out, DEFAULT_BUFFER_SIZE, DEFAULT_BUFFER_SIZE);
+		}
+
+		public FastIO(InputStream in, OutputStream out, int bufferSize) {
+			this(in, out, bufferSize, bufferSize);
+		}
+
+		public char nextChar() {
+			byte b;
+			while (isSpace(b = read()))
+				;
+
+			return (char) b;
+		}
+
+		public String next() {
+			byte b;
+			while (isSpace(b = read()))
+				;
+
+			int pos = 0;
+			do {
+				charBuffer[pos++] = (char) b;
+			} while (!isSpace(b = read()));
+
+			return new String(charBuffer, 0, pos);
+		}
+
+		public String nextLine() {
+			byte b;
+			int pos = 0;
+
+			while (!isLine(b = read()))
+				charBuffer[pos++] = (char) b;
+
+			return new String(charBuffer, 0, pos);
+		}
+
+		public int nextInt() {
+			byte b;
+			while (isSpace(b = read()))
+				;
+
+			boolean negative = false;
+			int result = b - '0';
+			if (b == '-') {
+				negative = true;
+				result = 0;
+			}
+
+			while (isDigit(b = read()))
+				result = (result * 10) + (b - '0');
+
+			return negative ? -result : result;
+		}
+
+		public long nextLong() {
+			byte b;
+			while (isSpace(b = read()))
+				;
+
+			boolean negative = false;
+			long result = b - '0';
+			if (b == '-') {
+				negative = true;
+				result = 0;
+			}
+
+			while (isDigit(b = read()))
+				result = (result * 10) + (b - '0');
+
+			return negative ? -result : result;
+		}
+
+		public float nextFloat() {
+			byte b;
+			while (isSpace(b = read()))
+				;
+
+			int pos = 0;
+			do {
+				charBuffer[pos++] = (char) b;
+			} while (!isSpace(b = read()));
+
+			return Float.parseFloat(new String(charBuffer, 0, pos));
+		}
+
+		public float nextFloat2() {
+			byte b;
+			while (isSpace(b = read()))
+				;
+
+			boolean negative = false;
+			float result = b - '0';
+			if (b == '-') {
+				negative = true;
+				result = 0;
+			}
+
+			while (isDigit(b = read()))
+				result = (result * 10) + (b - '0');
+
+			float d = 1;
+			if (b == '.') {
+				while (isDigit(b = read()))
+					result += (b - '0') / (d *= 10);
+			}
+
+			return negative ? -result : result;
+		}
+
+		public double nextDouble() {
+			byte b;
+			while (isSpace(b = read()))
+				;
+
+			int pos = 0;
+			do {
+				charBuffer[pos++] = (char) b;
+			} while (!isSpace(b = read()));
+
+			return Double.parseDouble(new String(charBuffer, 0, pos));
+		}
+
+		public double nextDouble2() {
+			byte b;
+			while (isSpace(b = read()))
+				;
+
+			boolean negative = false;
+			double result = b - '0';
+			if (b == '-') {
+				negative = true;
+				result = 0;
+			}
+
+			while (isDigit(b = read()))
+				result = (result * 10) + (b - '0');
+
+			double d = 1;
+			if (b == '.') {
+				while (isDigit(b = read()))
+					result += (b - '0') / (d *= 10);
+			}
+
+			return negative ? -result : result;
+		}
+
+		public char[] nextToCharArray() {
+			byte b;
+			while (isSpace(b = read()))
+				;
+
+			int pos = 0;
+			do {
+				charBuffer[pos++] = (char) b;
+			} while (!isSpace(b = read()));
+
+			char[] array = new char[pos];
+			System.arraycopy(charBuffer, 0, array, 0, pos);
+			return array;
+		}
+
+		public int[] nextIntArray(int size) {
+			int[] array = new int[size];
+			for (int i = 0; i < size; i++)
+				array[i] = nextInt();
+
+			return array;
+		}
+
+		public long[] nextLongArray(int size) {
+			long[] array = new long[size];
+			for (int i = 0; i < size; i++)
+				array[i] = nextLong();
+
+			return array;
+		}
+
+		public int[][] nextInt2DArray(int Y, int X) {
+			int[][] array = new int[Y][X];
+			for (int y = 0; y < Y; y++)
+				for (int x = 0; x < X; x++)
+					array[y][x] = nextInt();
+
+			return array;
+		}
+
+		public void print(char c) {
+			write((byte) c);
+		}
+
+		public void print(String s) {
+			for (int i = 0; i < s.length(); i++)
+				write((byte) s.charAt(i));
+		}
+
+		public void print(int i) {
+			if (i == 0) {
+				write((byte) '0');
+				return;
+			}
+			if (i == Integer.MIN_VALUE) {
+				write((byte) '-');
+				write((byte) '2');
+				write((byte) '1');
+				write((byte) '4');
+				write((byte) '7');
+				write((byte) '4');
+				write((byte) '8');
+				write((byte) '3');
+				write((byte) '6');
+				write((byte) '4');
+				write((byte) '8');
+				return;
+			}
+
+			if (i < 0) {
+				write((byte) '-');
+				i = -i;
+			}
+
+			int pos = 0;
+			while (i > 0) {
+				byteBuffer[pos++] = (byte) ((i % 10) + '0');
+				i /= 10;
+			}
+
+			while (pos-- > 0)
+				write(byteBuffer[pos]);
+		}
+
+		public void print(long l) {
+			if (l == 0) {
+				write((byte) '0');
+				return;
+			}
+			if (l == Long.MIN_VALUE) {
+				write((byte) '-');
+				write((byte) '9');
+				write((byte) '2');
+				write((byte) '2');
+				write((byte) '3');
+				write((byte) '3');
+				write((byte) '7');
+				write((byte) '2');
+				write((byte) '0');
+				write((byte) '3');
+				write((byte) '6');
+				write((byte) '8');
+				write((byte) '5');
+				write((byte) '4');
+				write((byte) '7');
+				write((byte) '7');
+				write((byte) '5');
+				write((byte) '8');
+				write((byte) '0');
+				write((byte) '8');
+				return;
+			}
+
+			if (l < 0) {
+				write((byte) '-');
+				l = -l;
+			}
+
+			int pos = 0;
+			while (l > 0) {
+				byteBuffer[pos++] = (byte) ((l % 10) + '0');
+				l /= 10;
+			}
+
+			while (pos-- > 0)
+				write(byteBuffer[pos]);
+		}
+
+		public void print(float f) {
+			String sf = Float.toString(f);
+			for (int i = 0; i < sf.length(); i++)
+				write((byte) sf.charAt(i));
+		}
+
+		public void print(double d) {
+			String sd = Double.toString(d);
+			for (int i = 0; i < sd.length(); i++)
+				write((byte) sd.charAt(i));
+		}
+
+		public void printls(char c) {
+			print(c);
+			write((byte) ' ');
+		}
+
+		public void printls(String s) {
+			print(s);
+			write((byte) ' ');
+		}
+
+		public void printls(int i) {
+			print(i);
+			write((byte) ' ');
+		}
+
+		public void printls(long l) {
+			print(l);
+			write((byte) ' ');
+		}
+
+		public void printls(float f) {
+			print(f);
+			write((byte) ' ');
+		}
+
+		public void printls(double d) {
+			print(d);
+			write((byte) ' ');
+		}
+
+		public void println(char c) {
+			print(c);
+			write((byte) '\n');
+		}
+
+		public void println(String s) {
+			print(s);
+			write((byte) '\n');
+		}
+
+		public void println(int i) {
+			print(i);
+			write((byte) '\n');
+		}
+
+		public void println(long l) {
+			print(l);
+			write((byte) '\n');
+		}
+
+		public void println(float f) {
+			print(f);
+			write((byte) '\n');
+		}
+
+		public void println(double d) {
+			print(d);
+			write((byte) '\n');
+		}
+
+		public void printf(String format, Object... args) {
+			String s = String.format(format, args);
+			for (int i = 0; i < s.length(); i++)
+				write((byte) s.charAt(i));
+		}
+
+		public void fprint(char c) {
+			print(c);
+			flushBuffer();
+		}
+
+		public void fprint(String s) {
+			print(s);
+			flushBuffer();
+		}
+
+		public void fprint(int i) {
+			print(i);
+			flushBuffer();
+		}
+
+		public void fprint(long l) {
+			print(l);
+			flushBuffer();
+		}
+
+		public void fprint(float f) {
+			print(f);
+			flushBuffer();
+		}
+
+		public void fprint(double d) {
+			print(d);
+			flushBuffer();
+		}
+
+		public void fprintf(String format, Object... args) {
+			printf(format, args);
+			flushBuffer();
+		}
+
+		private byte read() {
+			if (nextIn >= inLength) {
+				if ((inLength = fill()) == EOF)
+					return EOF;
+				nextIn = 0;
+			}
+
+			return inBuffer[nextIn++];
+		}
+
+		private void write(byte b) {
+			if (nextOut >= outBuffer.length)
+				flushBuffer();
+
+			outBuffer[nextOut++] = b;
+		}
+
+		private int fill() {
+			try {
+				return in.read(inBuffer, 0, inBuffer.length);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
 			}
 		}
 
-		return returnValue;
-	}
+		public void flush() {
+			if (nextOut == 0)
+				return;
 
-	// quick sort array
-	private static void quickSort(Node[] array, int low, int high) {
-		if (array == null || array.length == 0)
-			return;
-		if (low >= high)
-			return;
-
-		int middle = low + (high - low) / 2;
-		int pivot = array[middle].value;
-		int i = low, j = high;
-
-		while (i <= j) {
-			while (array[i].value < pivot)
-				i++;
-			while (array[j].value > pivot)
-				j--;
-
-			if (i <= j) {
-				Node temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				i++;
-				j--;
-			}
+			flushBuffer();
 		}
 
-		if (low < j)
-			quickSort(array, low, j);
-		if (high > i)
-			quickSort(array, i, high);
-	}
+		private void flushBuffer() {
+			try {
+				out.write(outBuffer, 0, nextOut);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
 
-	// save index
-	private static class Node {
-		public int value;
-		public int index;
+			nextOut = 0;
+		}
 
-		public Node(int value, int index) {
-			this.value = value;
-			this.index = index;
+		public void close() {
+			flush();
+		}
+
+		private boolean isDigit(byte b) {
+			return b >= '0' && b <= '9';
+		}
+
+		private boolean isLine(byte b) {
+			return b == '\n' || b == '\r' || b == EOF;
+		}
+
+		private boolean isSpace(byte b) {
+			return b == ' ' || b == '\t' || b == '\n' || b == '\r' || b == '\f' || b == EOF;
 		}
 	}
 }
