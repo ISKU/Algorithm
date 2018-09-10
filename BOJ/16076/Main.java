f0bf3ab7193af18313df3724d0718781847d5ff9
Minho Kim <minho.kim093@gmail.com>
2018-09-10 15:34:58 +0900

    BOJ #16076: 휴식이 필요해

---
+++ BOJ/16076/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: September 9, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/16076
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
