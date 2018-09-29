33e76ddf975320196d55ef39b22bc3144f38da8c
Minho Kim <minho.kim093@gmail.com>
2018-09-29 21:56:27 +0900

    BOJ #16170: 오늘의 날짜는?

---
+++ BOJ/16170/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: September 29, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/16170
+ */
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print("2018\n09\n29");
+	}
+}
