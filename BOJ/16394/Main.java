ee99c6bbf4c17a4d1d4fdedbaeb3b3d50a507f7b
Minho Kim <minho.kim093@gmail.com>
2018-11-11 10:38:29 +0900

    BOJ #16394: 홍익대학교

---
+++ BOJ/16394/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: November 11, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/16394
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.print(new Scanner(System.in).nextInt() - 1946);
+	}
+}
