ddbcace306c266b58974fd62ea836e353c40f8c5
Minho Kim <minho.kim093@gmail.com>
2018-11-12 16:59:43 +0900

    BOJ #16429: 2018 빼빼로 데이 이벤트 겸 스타트링크 홍보

---
+++ BOJ/16429/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: November 11, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/16429
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("1111");
+	}
+}
