459233d76f2ca9fada2981d650c2789a9921cb55
Minho Kim <minho.kim093@gmail.com>
2018-11-12 16:58:57 +0900

    BOJ #16430: 제리와 톰

---
+++ BOJ/16430/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: November 11, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/16430
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.printf("%d %d", B - A, B);
+	}
+}
