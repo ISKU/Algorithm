f6dfda8b8beadd1408b416637ab1b8dcaa03bd24
Minho Kim <minho.kim093@gmail.com>
2018-11-20 03:20:26 +0900

    BOJ #16485: 작도하자! - ②

---
+++ BOJ/16485/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: November 20, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/16485
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double C = sc.nextDouble();
+		double B = sc.nextDouble();
+		System.out.printf("%.6f", C / B);
+	}
+}
