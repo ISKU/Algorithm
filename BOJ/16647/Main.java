aa6a02cb890fa4059b92d7c18bef608e1074febc
Minho Kim <minho.kim093@gmail.com>
2018-12-08 09:46:21 +0900

    BOJ #16647: Happy Birthday, kipa00!

---
+++ BOJ/16647/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: December 4, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/16647
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++)
+			System.out.println("키파 생일 축하");
+	}
+}
