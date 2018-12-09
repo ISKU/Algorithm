0f5dd0ec7a5bb7c4b3446a545f1c24e6b174eab1
Minho Kim <minho.kim093@gmail.com>
2018-12-10 01:03:19 +0900

    BOJ #10989: 수 정렬하기 3

--- BOJ/10989/Main.java
+++ BOJ/10989/Main.java
@@ -1,62 +1,32 @@
 
-/* 
- * Author: Kim Min-Ho (ISKU)
- * Date: 2016.08.10
- * email: minho1a@hanmail.net
- * 
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: December 8, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
  * https://github.com/ISKU/Algorithm
  * https://www.acmicpc.net/problem/10989
  */
 
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String args[]) throws IOException {
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder output = new StringBuilder();
-		int arraySize = Integer.parseInt(input.readLine());
-		int[] arrayNumber = new int[arraySize];
 
-		for (int count = 0; count < arraySize; count++)
-			arrayNumber[count] = Integer.parseInt(input.readLine());
+	public static void main(String... args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
 
-		quickSort(arrayNumber, 0, arraySize - 1);
+		int[] array = new int[N];
+		for (int i = 0; i < N; i++)
+			array[i] = Integer.parseInt(br.readLine());
 
-		for (int index = 0; index < arraySize; index++)
-			output.append(arrayNumber[index] + "\n");
-		System.out.print(output);
-	}
-
-	private static void quickSort(int[] array, int low, int high) {
-		if (array == null || array.length == 0)
-			return;
-		if (low >= high)
-			return;
-
-		int middle = low + (high - low) / 2;
-		int pivot = array[middle];
-		int i = low, j = high;
-
-		while (i <= j) {
-			while (array[i] < pivot)
-				i++;
-			while (array[j] > pivot)
-				j--;
-
-			if (i <= j) {
-				int temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				i++;
-				j--;
-			}
+		Arrays.sort(array);
+		for (int i = 0; i < N; i++) {
+			bw.write(String.valueOf(array[i]));
+			bw.write('\n');
 		}
-
-		if (low < j)
-			quickSort(array, low, j);
-		if (high > i)
-			quickSort(array, i, high);
+		bw.close();
 	}
 }
