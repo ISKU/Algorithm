c11b1be7ba52ce3fd243b92bacbd4d023fdcaceb
Minho Kim <minho.kim093@gmail.com>
2018-12-10 01:02:27 +0900

    BOJ #2749: 피보나치 수 3

--- BOJ/2749/Main.java
+++ BOJ/2749/Main.java
@@ -1,31 +1,43 @@
-/* 
- * Author: Kim Min-Ho (ISKU)
- * Date: 2016.08.12
- * email: minho1a@hanmail.net
- * 
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: December 8, 2018
+ * E-mail: minho.kim093@gmail.com
+ *
  * https://github.com/ISKU/Algorithm
  * https://www.acmicpc.net/problem/2749
  */
 
-import java.util.Scanner;
-import java.math.BigInteger;
+import java.util.*;
+import java.math.*;
 
 public class Main {
 
-	private static final BigInteger pisanoPeriod = new BigInteger("1500000"); // 15*10^(n-1), n=6(10^6)
-	private static final BigInteger DIVISOR = new BigInteger("1000000");
+	private static final long MOD = 1_000_000;
+	private static final BigInteger TWO = BigInteger.valueOf(2);
 
-	public static void main(String args[]) {
-		BigInteger inputNumber = new BigInteger(new Scanner(System.in).next());
-		BigInteger first = new BigInteger("0");
-		BigInteger second = new BigInteger("1");
-		BigInteger fibonacciNumber = new BigInteger("1");
+	public static void main(String... args) {
+		BigInteger N = new Scanner(System.in).nextBigInteger().subtract(BigInteger.ONE);
 
-		for (int count = 1; count < Integer.parseInt(inputNumber.remainder(pisanoPeriod).toString()); count++) {
-			fibonacciNumber = first.add(second);
-			first = second.remainder(DIVISOR);
-			second = fibonacciNumber.remainder(DIVISOR);
+		long[][] mat = new long[][] { { 1, 0 }, { 0, 1 } };
+		long[][] fibo = new long[][] { { 1, 1, }, { 1, 0 } };
+		while (N.compareTo(BigInteger.ZERO) > 0) {
+			if (N.remainder(TWO).compareTo(BigInteger.ONE) == 0)
+				mat = mult(mat, fibo);
+			fibo = mult(fibo, fibo);
+			N = N.divide(TWO);
 		}
-		System.out.println(fibonacciNumber.remainder(DIVISOR));
+
+		System.out.print(mat[0][0]);
+	}
+
+	private static long[][] mult(long[][] A, long[][] B) {
+		long[][] mat = new long[2][2];
+
+		for (int i = 0; i < 2; i++)
+			for (int j = 0; j < 2; j++)
+				for (int k = 0; k < 2; k++)
+					mat[i][j] = (mat[i][j] + (A[i][k] * B[k][j]) % MOD) % MOD;
+
+		return mat;
 	}
 }
