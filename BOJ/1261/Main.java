aa7fad293e94968ac355dfa9908a2a364bf2b998
Minho Kim <minho.kim093@gmail.com>
2018-12-10 00:47:36 +0900

    BOJ #1261: 알고스팟

--- BOJ/1261/Main.java
+++ BOJ/1261/Main.java
@@ -1,83 +1,82 @@
 /*
  * Author: Minho Kim (ISKU)
- * Date: January 24, 2018
- * E-mail: minho1a@hanmail.net
+ * Date: December 8, 2018
+ * E-mail: minho.kim093@gmail.com
  *
  * https://github.com/ISKU/Algorithm
  * https://www.acmicpc.net/problem/1261
  */
 
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
+	public static final int[] dy = new int[] { -1, 0, 1, 0 };
+	public static final int[] dx = new int[] { 0, 1, 0, -1 };
+
+	private static int[][] map;
 	private static int Y, X;
-	private static int[][] graph;
 
-	public static void main(String... args) {
-		Scanner sc = new Scanner(System.in);
-		X = sc.nextInt();
-		Y = sc.nextInt();
+	public static void main(String... args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		X = Integer.parseInt(st.nextToken());
+		Y = Integer.parseInt(st.nextToken());
 
-		graph = new int[Y][X];
+		map = new int[Y][X];
 		for (int y = 0; y < Y; y++) {
-			String[] line = sc.next().split("");
+			String line = br.readLine();
 			for (int x = 0; x < X; x++)
-				graph[y][x] = Integer.parseInt(line[x]);
+				map[y][x] = line.charAt(x) - '0';
 		}
 
-		int[][] dist = dijkstra(0, 0);
-		System.out.print(dist[Y - 1][X - 1]);
+		System.out.print(dijkstra());
 	}
 
-	private static int[][] dijkstra(int startY, int startX) {
-		PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
+	private static int dijkstra() {
+		Queue<Edge> pq = new PriorityQueue<>(new Comparator<Edge>() {
+			@Override
+			public int compare(Edge o1, Edge o2) {
+				return Integer.compare(o1.weight, o2.weight);
+			}
+		});
 		int[][] dist = new int[Y][X];
-		for (int y = 0; y < Y; y++)
-			Arrays.fill(dist[y], -1);
-
-		dist[startY][startX] = 0;
-		pq.add(new Edge(startY, startX, 0));
+		for (int i = 0; i < Y; i++)
+			Arrays.fill(dist[i], Integer.MAX_VALUE);
+		dist[0][0] = 0;
+		pq.add(new Edge(0, 0, 0));
 
 		while (!pq.isEmpty()) {
-			Edge edge = pq.poll();
-			int y = edge.y;
-			int x = edge.x;
+			Edge e = pq.poll();
+			if (dist[e.y][e.x] < e.weight)
+				continue;
 
-			if (y - 1 >= 0 && (dist[y - 1][x] == -1 || dist[y - 1][x] > graph[y - 1][x] + dist[y][x])) {
-				dist[y - 1][x] = graph[y - 1][x] + dist[y][x];
-				pq.add(new Edge(y - 1, x, graph[y - 1][x] + dist[y][x]));
-			}
-			if (y + 1 < Y && (dist[y + 1][x] == -1 || dist[y + 1][x] > graph[y + 1][x] + dist[y][x])) {
-				dist[y + 1][x] = graph[y + 1][x] + dist[y][x];
-				pq.add(new Edge(y + 1, x, graph[y + 1][x] + dist[y][x]));
-			}
-			if (x - 1 >= 0 && (dist[y][x - 1] == -1 || dist[y][x - 1] > graph[y][x - 1] + dist[y][x])) {
-				dist[y][x - 1] = graph[y][x - 1] + dist[y][x];
-				pq.add(new Edge(y, x - 1, graph[y][x - 1] + dist[y][x]));
-			}
-			if (x + 1 < X && (dist[y][x + 1] == -1 || dist[y][x + 1] > graph[y][x + 1] + dist[y][x])) {
-				dist[y][x + 1] = graph[y][x + 1] + dist[y][x];
-				pq.add(new Edge(y, x + 1, graph[y][x + 1] + dist[y][x]));
+			for (int i = 0; i < 4; i++) {
+				int y = e.y + dy[i];
+				int x = e.x + dx[i];
+				if (y < 0 || y >= Y || x < 0 || x >= X)
+					continue;
+
+				int cost = e.weight + map[y][x];
+				if (cost < dist[y][x]) {
+					dist[y][x] = cost;
+					pq.add(new Edge(y, x, cost));
+				}
 			}
 		}
 
-		return dist;
+		return dist[Y - 1][X - 1];
 	}
 
-	private static class Edge implements Comparable<Edge> {
+	private static class Edge {
 		public int y, x;
 		public int weight;
 
-		public Edge(int y, int x, int wegiht) {
+		public Edge(int y, int x, int weight) {
 			this.y = y;
 			this.x = x;
 			this.weight = weight;
-		}
-
-		@Override
-		public int compareTo(Edge e) {
-			return this.weight - e.weight;
 		}
 	}
 }
