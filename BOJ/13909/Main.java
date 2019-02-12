27897f08047c82c55167c23035919a1ad4fffd33
Minho Kim <minho.kim093@gmail.com>
2019-02-12 23:35:36 +0900

    BOJ #13909: 창문 닫기

---
+++ BOJ/13909/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: February 12, 2019
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/13909
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String... args) {
+		System.out.print((int) Math.sqrt(new Scanner(System.in).nextLong()));
+	}
+}
