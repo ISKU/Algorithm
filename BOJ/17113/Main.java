41c6fdd77137579e2c7334fc4f050e973731f388
Minho Kim <minho.kim093@gmail.com>
2019-04-01 02:00:34 +0900

    BOJ #17113: 3분 그래프

---
+++ BOJ/17113/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: March 31, 2019
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/17113
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("구데기 3분 그래프");
+	}
+}
