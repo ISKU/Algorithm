694cf988031ff63b5ce735b5504b213a6c85fe07
Minho Kim <minho.kim093@gmail.com>
2019-04-01 02:00:40 +0900

    BOJ #17117: 평가

---
+++ BOJ/17117/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: March 31, 2019
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/17117
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int R = sc.nextInt();
+
+		if (R == 12)
+			System.out.println("21076.0");
+		if (R == 16)
+			System.out.println("2.718281828459045");
+		if (R == 14)
+			System.out.println("0.523598775598299");
+	}
+}
