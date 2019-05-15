592be5a61e2457e64156c9bee0f4783041fbf3c9
Minho Kim <minho.kim093@gmail.com>
2019-05-16 05:04:56 +0900

    BOJ #11921: 0.1

--- BOJ/11921/Main.java
+++ BOJ/11921/Main.java
@@ -1,6 +1,6 @@
 /*
  * Author: Minho Kim (ISKU)
- * Date: December 8, 2018
+ * Date: May 16, 2019
  * E-mail: minho.kim093@gmail.com
  *
  * https://github.com/ISKU/Algorithm
@@ -8,8 +8,10 @@
  */
 
 public class Main {
+
+	private static byte[] buffer = new byte[8000000];
+
 	public static void main(String[] args) throws Exception {
-		byte[] buffer = new byte[7200000];
 		int next = 8;
 		int i = 0;
 		long sum = 0;
@@ -17,7 +19,7 @@ public class Main {
 		byte b;
 
 		System.in.read(buffer, 0, buffer.length);
-		for (; i < 900000; i++) {
+		for (; i < 1000000; i++) {
 			n = buffer[next++] - '0';
 			while ((b = buffer[next++]) != '\n')
 				n = n * 10 + b - '0';
@@ -25,14 +27,15 @@ public class Main {
 			sum += n;
 		}
 
-		next = 7;
-		buffer[0] = '9';
+		next = 8;
+		buffer[0] = '1';
 		buffer[1] = '0';
 		buffer[2] = '0';
 		buffer[3] = '0';
 		buffer[4] = '0';
 		buffer[5] = '0';
-		buffer[6] = '\n';
+		buffer[6] = '0';
+		buffer[7] = '\n';
 
 		n = 1000;
 		while (sum > 0) {
