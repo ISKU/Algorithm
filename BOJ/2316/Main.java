e07f28e489a4c0b18e63cdd68621fa4d770c876b
Minho Kim <minho.kim093@gmail.com>
2019-05-27 06:56:27 +0900

    BOJ #2316: 도시 왕복하기

--- BOJ/2316/Main.java
+++ BOJ/2316/Main.java
@@ -1,85 +1,92 @@
-/* 
- * Author: Kim Min-Ho (ISKU)
- * Date: 2017.02.25
- * Email: minho1a@hanmail.net
- * 
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: May 27, 2019
+ * E-mail: minho.kim093@gmail.com
+ *
  * https://github.com/ISKU/Algorithm
  * https://www.acmicpc.net/problem/2316
  */
 
-import java.util.Scanner;
-import java.util.Vector;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	private static Vector<Vector<Integer>> graph;
-	private static int[][] capacity;
-	private static int[][] flow;
+	private static List<Integer>[] network;
+	private static int[][] capacity, flow;
+	private static int source, sink, vertices;
 
-	public static void main(String... args) {
-		Scanner input = new Scanner(System.in);
-		int N = input.nextInt();
-		int P = input.nextInt();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
 
-		capacity = new int[N * 2 + 1][N * 2 + 1];
-		flow = new int[N * 2 + 1][N * 2 + 1];
-		graph = new Vector<Vector<Integer>>();
-		for (int i = 0; i < N * 2 + 1; i++)
-			graph.add(new Vector<Integer>());
+		vertices = N * 2 + 1;
+		source = 1 + N;
+		sink = 2;
+		capacity = new int[vertices][vertices];
+		flow = new int[vertices][vertices];
+		network = new ArrayList[vertices];
+		for (int i = 1; i < vertices; i++)
+			network[i] = new ArrayList<Integer>();
 
 		for (int u = 1; u <= N; u++) {
-			graph.get(u).add(u + N);
+			network[u].add(u + N);
+			network[u + N].add(u);
 			capacity[u][u + N] = 1;
 		}
 		while (P-- > 0) {
-			int u = input.nextInt();
-			int v = input.nextInt();
-			graph.get(u + N).add(v);
-			graph.get(v + N).add(u);
-			capacity[u + N][v] = 1;
-			capacity[v + N][u] = 1;
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			network[u + N].add(v);
+			network[v].add(u + N);
+			network[v + N].add(u);
+			network[u].add(v + N);
+			capacity[u + N][v] += 1;
+			capacity[v + N][u] += 1;
 		}
 
-		int total = 0;
-		int source = 1 + N, sink = 2;
+		System.out.print(edmondskarp());
+	}
+
+	private static int edmondskarp() {
+		Queue<Integer> q = new LinkedList<>();
+		int[] path = new int[vertices];
+
+		int mf = 0;
 		while (true) {
-			Queue<Integer> queue = new LinkedList<Integer>();
-			int[] prev = new int[N * 2 + 1];
-			Arrays.fill(prev, -1);
+			q.clear();
+			Arrays.fill(path, 0);
 
-			queue.add(source);
-			while (!queue.isEmpty()) {
-				int cur = queue.poll();
+			q.add(source);
+			while (!q.isEmpty()) {
+				int u = q.poll();
+				if (u == sink)
+					break;
 
-				for (int dest : graph.get(cur)) {
-					if (capacity[cur][dest] - flow[cur][dest] > 0 && prev[dest] == -1) {
-						queue.add(dest);
-						prev[dest] = cur;
-
-						if (dest == sink)
-							break;
+				for (int v : network[u]) {
+					if (path[v] == 0 && capacity[u][v] > flow[u][v]) {
+						q.add(v);
+						path[v] = u;
 					}
 				}
 			}
-
-			if (prev[sink] == -1)
+			if (path[sink] == 0)
 				break;
 
-			int curflow = Integer.MAX_VALUE;
-			for (int i = sink; i != source; i = prev[i])
-				curflow = Math.min(curflow, capacity[prev[i]][i] - flow[prev[i]][i]);
-
-			for (int i = sink; i != source; i = prev[i]) {
-				flow[prev[i]][i] += curflow;
-				flow[i][prev[i]] -= curflow;
+			int cf = Integer.MAX_VALUE;
+			for (int v = sink; v != source; v = path[v])
+				cf = Math.min(cf, capacity[path[v]][v] - flow[path[v]][v]);
+			for (int v = sink; v != source; v = path[v]) {
+				flow[path[v]][v] += cf;
+				flow[v][path[v]] -= cf;
 			}
 
-			total += curflow;
+			mf += cf;
 		}
 
-		System.out.print(total);
+		return mf;
 	}
 }
