ffab7bd44e4d4e02d76044463c14002e6744a929
Minho Kim <minho.kim093@gmail.com>
2019-05-30 16:34:12 +0900

    BOJ #5651: 완전 중요한 간선

--- BOJ/5651/Main.java
+++ BOJ/5651/Main.java
@@ -1,116 +1,143 @@
-/* 
- * Author: Kim Min-Ho (ISKU)
- * Date: 2017.03.02
- * Email: minho1a@hanmail.net
- * 
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: May 28, 2019
+ * E-mail: minho.kim093@gmail.com
+ *
  * https://github.com/ISKU/Algorithm
  * https://www.acmicpc.net/problem/5651
  */
 
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	private static ArrayList<ArrayList<Integer>> network;
-	private static Queue<Integer> queue;
-	private static int[][] capacity;
-	private static int[][] flow;
-	private static int[][] table;
-	private static int[] prev;
+	private static List<Edge>[] network;
+	private static Edge[] edges;
+	private static int source, sink, N, M;
 
-	public static void main(String... args) {
-		Scanner input = new Scanner(System.in);
-		int testCase = input.nextInt();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		while (testCase-- > 0) {
-			int N = input.nextInt();
-			int M = input.nextInt();
+		int T = Integer.parseInt(br.readLine());
+		while (T-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
 
-			queue = new LinkedList<Integer>();
-			prev = new int[N + 1];
-			capacity = new int[N + 1][N + 1];
-			flow = new int[N + 1][N + 1];
-			table = new int[M + 1][2];
-			network = new ArrayList<ArrayList<Integer>>();
-			for (int i = 0; i < N + 1; i++)
-				network.add(new ArrayList<Integer>());
+			source = 1;
+			sink = N;
+			network = new ArrayList[N + 1];
+			for (int i = 1; i <= N; i++)
+				network[i] = new ArrayList<Edge>();
 
-			for (int i = 1; i <= M; i++) {
-				int u = input.nextInt();
-				int v = input.nextInt();
-				int w = input.nextInt();
-				network.get(u).add(v);
-				network.get(v).add(u);
-				capacity[u][v] += w;
-				table[i][0] = u;
-				table[i][1] = v;
+			edges = new Edge[M];
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int u = Integer.parseInt(st.nextToken());
+				int v = Integer.parseInt(st.nextToken());
+				int capacity = Integer.parseInt(st.nextToken());
+				edges[i] = add(u, v, capacity);
 			}
 
-			int source = 1, sink = N;
-			while (true) {
-				Arrays.fill(prev, -1);
+			maximumFlow();
+			bw.write(bfs() + "\n");
+		}
 
-				queue.add(source);
-				while (!queue.isEmpty()) {
-					int cur = queue.poll();
+		bw.close();
+	}
 
-					for (int dest : network.get(cur)) {
-						if (capacity[cur][dest] - flow[cur][dest] > 0 && prev[dest] == -1) {
-							queue.add(dest);
-							prev[dest] = cur;
+	private static void maximumFlow() {
+		Queue<Integer> q = new LinkedList<>();
+		Edge[] path = new Edge[N + 1];
 
-							if (dest == sink)
-								break;
-						}
-					}
-				}
+		while (true) {
+			q.clear();
+			Arrays.fill(path, null);
 
-				if (prev[sink] == -1)
+			q.add(source);
+			while (!q.isEmpty()) {
+				int u = q.poll();
+				if (u == sink)
 					break;
 
-				int curflow = Integer.MAX_VALUE;
-				for (int i = sink; i != source; i = prev[i])
-					curflow = Math.min(curflow, capacity[prev[i]][i] - flow[prev[i]][i]);
-
-				for (int i = sink; i != source; i = prev[i]) {
-					flow[prev[i]][i] += curflow;
-					flow[i][prev[i]] -= curflow;
-				}
-			}
-
-			int total = 0;
-			for (int i = 1; i <= M; i++) {
-				source = table[i][0];
-				sink = table[i][1];
-
-				if (flow[source][sink] != capacity[source][sink])
-					continue;
-
-				Arrays.fill(prev, -1);
-				queue.add(source);
-				while (!queue.isEmpty()) {
-					int cur = queue.poll();
-
-					for (int dest : network.get(cur)) {
-						if (capacity[cur][dest] - flow[cur][dest] > 0 && prev[dest] == -1) {
-							queue.add(dest);
-							prev[dest] = cur;
-
-							if (dest == sink)
-								break;
-						}
+				for (Edge edge : network[u]) {
+					int v = edge.v;
+					if (path[v] == null && edge.capacity > edge.flow) {
+						q.add(v);
+						path[v] = edge;
 					}
 				}
+			}
+			if (path[sink] == null)
+				break;
 
-				if (prev[sink] == -1)
-					total++;
+			int cf = Integer.MAX_VALUE;
+			for (int v = sink; v != source; v = path[v].u)
+				cf = Math.min(cf, path[v].capacity - path[v].flow);
+			for (int v = sink; v != source; v = path[v].u) {
+				path[v].flow += cf;
+				path[v].back.flow -= cf;
+			}
+		}
+	}
+
+	private static int bfs() {
+		Queue<Integer> q = new LinkedList<>();
+		boolean[] visited = new boolean[N + 1];
+
+		int link = 0;
+		for (Edge candidate : edges) {
+			if (candidate.capacity > candidate.flow)
+				continue;
+			candidate.capacity--;
+
+			q.clear();
+			Arrays.fill(visited, false);
+			q.add(candidate.u);
+			visited[candidate.u] = true;
+			while (!q.isEmpty()) {
+				int u = q.poll();
+				if (u == candidate.v)
+					break;
+
+				for (Edge edge : network[u]) {
+					int v = edge.v;
+					if (!visited[v] && edge.capacity > edge.flow) {
+						q.add(v);
+						visited[v] = true;
+					}
+				}
 			}
 
-			System.out.println(total);
+			if (!visited[candidate.v])
+				link++;
+			candidate.capacity++;
+		}
+
+		return link;
+	}
+
+	private static Edge add(int u, int v, int capacity) {
+		Edge uv = new Edge(u, v, capacity);
+		Edge vu = new Edge(v, u, 0);
+		uv.back = vu;
+		vu.back = uv;
+		network[u].add(uv);
+		network[v].add(vu);
+		return uv;
+	}
+
+	private static class Edge {
+		public int u, v;
+		public int capacity, flow;
+		public Edge back;
+
+		public Edge(int u, int v, int capacity) {
+			this.u = u;
+			this.v = v;
+			this.capacity = capacity;
 		}
 	}
 }
