100a8f571b55b843817d7c42ff165c870e098f32
Minho Kim <minho.kim093@gmail.com>
2019-06-02 03:34:35 +0900

    BOJ #17253: 삼삼한 수 2

---
+++ BOJ/17253/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: June 1, 2019
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/17253
+ */
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Long.parseLong(br.readLine());
+		if (N == 0)
+			exit();
+
+		while (N > 0) {
+			if (N % 3 == 2)
+				exit();
+			N /= 3;
+		}
+
+		System.out.print("YES");
+	}
+
+	private static void exit() {
+		System.out.print("NO");
+		System.exit(0);
+	}
+}
