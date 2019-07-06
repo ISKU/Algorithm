65ff4356fc044a6551bb8c6fc42fd4e3a4b25e25
Minho Kim <minho.kim093@gmail.com>
2019-07-06 22:21:11 +0900

    BOJ #17295: 엔드게임 스포일러

---
+++ BOJ/17295/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: July 6, 2019
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/17295
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Avengers: Endgame");
+	}
+}
