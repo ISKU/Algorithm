e9bd2c72ac95c4881ecdeb6ad426b0c191f640d9
Minho Kim <minho.kim093@gmail.com>
2019-12-02 04:30:12 +0900

    BOJ #18108: 1998년생인 내가 태국에서는 2541년생?!

---
+++ BOJ/18108/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: December 2, 2019
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/18108
+ */
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int year = Integer.parseInt(br.readLine());
+
+		System.out.println((year - 543));
+	}
+}
