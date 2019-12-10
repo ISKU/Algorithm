36062d91a07ecc191faa06d4584342fcd3a1a344
Minho Kim <minho.kim093@gmail.com>
2019-12-11 01:38:34 +0900

    BOJ #15439: Vera and Outfits

---
+++ BOJ/15439/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: December 9, 2019
+ * E-mail: minho.kim093@gmail.com
+ *
+ * https://github.com/ISKU/Algorithm
+ * https://www.acmicpc.net/problem/15439
+ */
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		System.out.println(N * (N - 1));
+	}
+}
