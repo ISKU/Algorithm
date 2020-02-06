8935797e1b3cf4e68d95b12b0aec20ac6c9b2dcc
Minho Kim <minho.kim093@gmail.com>
2020-02-06 22:52:08 +0900

    BOJ #2108: 통계학

--- BOJ/2108/Main.java
+++ BOJ/2108/Main.java
@@ -1,50 +1,64 @@
-/* 
- * Author: Kim Min-Ho (ISKU)
- * Date: 2017.02.11
- * Email: minho1a@hanmail.net
- * 
+/*
+ * Author: Minho Kim (ISKU)
+ * Date: February 6, 2020
+ * E-mail: minho.kim093@gmail.com
+ *
  * https://github.com/ISKU/Algorithm
  * https://www.acmicpc.net/problem/2108
  */
 
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String... args) {
-		Scanner input = new Scanner(System.in);
-		int[] array = new int[input.nextInt()];
-		int[] freq = new int[8001];
-		float sum = 0;
+	public static void main(String... args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		Node[] nodes = new Node[8001];
+		int[] array = new int[N];
+		for (int i = 0; i <= 8000; i++)
+			nodes[i] = new Node(i - 4000, 0);
+		for (int i = 0; i < N; i++) {
+			int n = Integer.parseInt(br.readLine());
+			array[i] = n;
+			nodes[n + 4000].count++;
+		}
+
+		double sum = 0;
+		for (int i = 0; i <= 8000; i++)
+			sum += nodes[i].number * nodes[i].count;
+		System.out.println(String.format("%.0f", sum / N));
+
+		Arrays.sort(array);
+		System.out.println(array[N / 2]);
+
+		Arrays.sort(nodes, new Comparator<Node>() {
+			@Override
+			public int compare(Node o1, Node o2) {
+				if (o1.count == o2.count)
+					return Integer.compare(o2.number, o1.number);
+				return Integer.compare(o1.count, o2.count);
+			}
+		});
+		System.out.println((nodes[8000].count == nodes[7999].count) ? nodes[7999].number : nodes[8000].number);
+
 		int max = Integer.MIN_VALUE;
 		int min = Integer.MAX_VALUE;
-
-		for (int i = 0; i < array.length; i++) {
-			array[i] = input.nextInt();
-			sum += array[i];
-			freq[array[i] + 4000]++;
+		for (int i = 0; i < N; i++) {
 			max = Math.max(max, array[i]);
 			min = Math.min(min, array[i]);
 		}
-
-		ArrayList<Integer> rFreq = new ArrayList<Integer>();
-		int vFreq = freq[0];
-		for (int i = 0; i < freq.length; i++)
-			vFreq = Math.max(vFreq, freq[i]);
-
-		for (int i = 0; i < freq.length; i++)
-			if (vFreq == freq[i])
-				rFreq.add(i);
-
-		int[] copy = array.clone();
-		Arrays.sort(copy);
-		Collections.sort(rFreq);
-
-		System.out.printf("%.0f\n", sum / array.length);
-		System.out.println(copy[array.length / 2]);
-		System.out.println(rFreq.size() >= 2 ? rFreq.get(1) - 4000 : rFreq.get(0) - 4000);
 		System.out.println(max - min);
+	}
+
+	private static class Node {
+		public int number;
+		public int count;
+
+		public Node(int number, int count) {
+			this.number = number;
+			this.count = count;
+		}
 	}
 }
